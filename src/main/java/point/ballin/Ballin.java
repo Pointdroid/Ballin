f2d35e8e1e8e4c933e22d49b769a3f834acd85bf
Rihards Ronis <79004832+Pointdroid@users.noreply.github.com>
2025-04-13 12:30:01 +0300

    Added right click test

--- src/main/java/point/ballin/Ballin.java
+++ src/main/java/point/ballin/Ballin.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
@@ -14,7 +15,9 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
@@ -26,6 +29,15 @@ public class Ballin implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	private static ActionResult interact(PlayerEntity player, World world, Hand hand) {
+		if (!world.isClient && (player.getMainHandStack().getItem() == Items.DIAMOND)) {
+			// Your logic here, e.g., sending a message to the player
+			((ServerPlayerEntity) player).sendMessage(Text.literal("Right-click detected!"), false);
+		}
+		return ActionResult.PASS;
+	}
+
 	public static class ModItems {
 		public static final RegistryKey<Item> BALL_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Ballin.MOD_ID, "basketball"));
 		public static final Item BASKETBALL = ModItems.register(
@@ -56,13 +68,7 @@ public class Ballin implements ModInitializer {
 		// Proceed with mild caution.
 		ModItems.initialize();
 		LOGGER.info("Hello Fabric world!");
-		UseItemCallback.EVENT.register((player, world, hand) -> {
-			if (!world.isClient && (player.getMainHandStack().getItem() == Items.DIAMOND)) {
-				// Your logic here, e.g., sending a message to the player
-				((ServerPlayerEntity) player).sendMessage(Text.literal("Right-click detected!"), false);
-			}
-			return ActionResult.PASS;
-		});
+		UseItemCallback.EVENT.register(Ballin::interact);
 	}
 }
 
